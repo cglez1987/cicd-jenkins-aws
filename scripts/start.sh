3d7ec9e2dab75f4f14704710db90b64cd4c008e7
cglez1987 <cglez1987@gmailcom>
2020-09-27 18:27:44 -0400

    Adding new code

--- scripts/start.sh
+++
@@ -1,4 +0,0 @@
-#!bin/bash
-
-cd /tmp
-java -jar TestSpringBootWithRDSMySQL-1.0-SNAPSHOT-spring-boot.jar
--- scripts/stop.sh
+++
@@ -1,3 +0,0 @@
-#!bin/bash
-
-kill -9 $(ps -aux | grep -v grep | grep TestSpring | awk '{print $2}')
